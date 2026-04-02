9deced4ce21a0f286385432957a38014666ea074
Eisenbahnfan <hendrik.noack@protonmail.com>
2026-04-02 15:43:09 +0200

    Add framebuffer to Galaxy Tab Pro
    This adds working framebuffer with bootmenu and navigation via buttons.

--- include/configs/exynos5420-galaxy-tab-common.h
+++ include/configs/exynos5420-galaxy-tab-common.h
@@ -9,7 +9,6 @@
 #define __CONFIG_EXYNOS5420_GALAXY_TAB_H
 
 #include <configs/exynos5420-common.h>
-#include <configs/exynos5-dt-common.h>
 #include <configs/exynos5-common.h>
 
 #define CFG_SYS_SDRAM_BASE	0x20000000
@@ -19,5 +18,24 @@
 
 #define CFG_SYS_INIT_RAM_ADDR	0x20000000
 #define CFG_SYS_INIT_RAM_SIZE	(512UL << 20UL)
+
+#undef BOOT_TARGET_DEVICES
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 2)
+
+#undef MEM_LAYOUT_ENV_SETTINGS
+#define MEM_LAYOUT_ENV_SETTINGS \
+	"bootm_size=0x10000000\0" \
+	"kernel_addr_r=0x22000000\0" \
+	"fdt_addr_r=0x23000000\0" \
+	"ramdisk_addr_r=0x23300000\0" \
+	"scriptaddr=0x30000000\0" \
+	"pxefile_addr_r=0x31000000\0"
+
+#undef CFG_EXTRA_ENV_SETTINGS
+#define CFG_EXTRA_ENV_SETTINGS \
+	EXYNOS_FDTFILE_SETTING \
+	MEM_LAYOUT_ENV_SETTINGS \
+	BOOTENV
 
 #endif	/* __CONFIG_EXYNOS5420_GALAXY_TAB_H */
--- include/configs/n2awifi.h
+++ include/configs/n2awifi.h
@@ -8,13 +8,7 @@
 #ifndef __CONFIG_N2AWIFI_H
 #define __CONFIG_N2AWIFI_H
 
-#define MEM_LAYOUT_ENV_SETTINGS \
-	"bootm_size=0x10000000\0" \
-	"kernel_addr_r=0x22000000\0" \
-	"fdt_addr_r=0x23000000\0" \
-	"ramdisk_addr_r=0x23300000\0" \
-	"scriptaddr=0x30000000\0" \
-	"pxefile_addr_r=0x31000000\0" \
+#define EXYNOS_FDTFILE_SETTING \
 	"fdtfile=exynos5420-n2awifi.dtb\0"
 
 #include <configs/exynos5420-galaxy-tab-common.h>
