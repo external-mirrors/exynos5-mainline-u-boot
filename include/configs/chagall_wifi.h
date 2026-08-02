25ab69b1969f543a21ea0c493ee879fe045831a8
Eisenbahnfan <hendrik.noack@protonmail.com>
2026-08-02 18:00:38 +0200

    Add chagallwifi

---
+++ include/configs/chagall_wifi.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2025 Hendrik Noack
+ *
+ * Configuration settings for the Samsung Exynos5420 Galaxy Note 10.1 (2014) WiFi board.
+ */
+
+#ifndef __CONFIG_N1AWIFI_H
+#define __CONFIG_N1AWIFI_H
+
+#define EXYNOS_FDTFILE_SETTING \
+	"fdtfile=exynos5420-chagall-wifi.dtb\0"
+
+#include <configs/exynos5420-galaxy-tab-common.h>
+
+#endif	/* __CONFIG_N2AWIFI_H */
